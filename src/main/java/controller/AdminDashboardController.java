fe2fdb5a5320e19d8d31fbb6fa8f294c2cd3d380
Ikeshala <ikeshala.peiris@gmail.com>
2024-01-06 22:25:00 +0530

    Created the Admin Dashboard view and developed/modified the AdminDashboardController.

---
+++ src/main/java/controller/AdminDashboardController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AdminDashboardController {
+
+    @FXML
+    private AnchorPane adminDashboardPane;
+
+    @FXML
+    void inventoryButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void itemCatalogButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logoutButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) adminDashboardPane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Login.fxml"))));
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            stage.setTitle("Login");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void orderPlacementButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void orderProcessingButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportsButtonOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void userManagementButtonOnAction(ActionEvent event) {
+
+    }
+
+}
--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,10 +4,17 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
+    public AnchorPane loginPane;
     @FXML
     private JFXTextField txtEmail;
 
@@ -25,7 +32,16 @@ public class LoginController {
 
     @FXML
     void LoginButtonOnAction(ActionEvent event) {
-
+        Stage stage = (Stage) loginPane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminDashboard.fxml"))));
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            stage.setTitle("Admin Dashboard");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
