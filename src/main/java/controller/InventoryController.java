8940f26983b47876b79021b665846345484798ca
Ikeshala <ikeshala.peiris@gmail.com>
2024-01-09 10:18:33 +0530

    Created the Inventory view, and developed/modified the InventoryController.

---
+++ src/main/java/controller/InventoryController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class InventoryController {
+
+    @FXML
+    private AnchorPane inventoryPane;
+
+    @FXML
+    private JFXTreeTableView<?> tblItem;
+
+    @FXML
+    private TreeTableColumn<?, ?> colCode;
+
+    @FXML
+    private TreeTableColumn<?, ?> colItemName;
+
+    @FXML
+    private TreeTableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TreeTableColumn<?, ?> colAction;
+
+    @FXML
+    void backButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) inventoryPane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminDashboard.fxml"))));
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            stage.setTitle("Login");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void logoutButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) inventoryPane.getScene().getWindow();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Login.fxml"))));
+            stage.centerOnScreen();
+            stage.setResizable(false);
+            stage.setTitle("Login");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
