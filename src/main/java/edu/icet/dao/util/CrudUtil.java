c913e134c4e5b6660709cbf727416e7375f84e03
Ikeshala <ikeshala.peiris@gmail.com>
2024-01-21 15:09:57 +0530

    Implemented CrudUtil

---
+++ src/main/java/edu/icet/dao/util/CrudUtil.java
@@ -0,0 +1,21 @@
+package edu.icet.dao.util;
+
+import edu.icet.db.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T>T execute(String sql, Object...args) throws SQLException, ClassNotFoundException {
+        PreparedStatement pstm = DBConnection.getInstance().getConnection().prepareStatement(sql);
+        if (sql.startsWith("SELECT") || sql.startsWith("select")){
+            return(T) pstm.executeQuery();
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            pstm.setObject((i+1), args[i]);
+        }
+
+        return (T)(Boolean) (pstm.executeUpdate()>0);
+    }
+}
